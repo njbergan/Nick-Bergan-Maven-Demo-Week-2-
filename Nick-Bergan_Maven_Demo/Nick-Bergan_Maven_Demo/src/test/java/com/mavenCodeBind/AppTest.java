7bce0a0f8a7ebb27c497d0b723d32c2a841ce58d
njbergan <35711090+njbergan@users.noreply.github.com>
2018-01-30 16:35:45 -0600

    Add files via upload
    Unzipped copy of maven-java demo for week 2

---
+++ Nick-Bergan_Maven_Demo/Nick-Bergan_Maven_Demo/src/test/java/com/mavenCodeBind/AppTest.java
@@ -0,0 +1,22 @@
+//Nick Bergan
+//This program is a part of maven, it calls main java files and tests them.
+package com.mavenCodeBind;
+
+//import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		
+		
+			com.mavenCodeBind.Hello_World.main(null);
+			
+			//fail("Test message");
+		
+		
+	}
+
+}
